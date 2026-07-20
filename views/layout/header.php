c6d3ebdc0b3e6be215c50ceba65f78b6b74c1e5b
ComuneMontelupoFiorentino <e.gulli@comune.montelupo-fiorentino.fi.it>
2026-07-20 16:50:58 +0200

    Update header.php

--- views/layout/header.php
+++ views/layout/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="index,follow">
     <title><?= NAME_SERVICE ?></title>    
-    <meta name="description" content="Comune in Chiaro è il portale del <?= ENTE ?> che consente di consultare servizi e informazionibasati sugli opendata comunali">
+    <meta name="description" content="Comune in Chiaro è il portale che consente di consultare servizi e informazioni basati sugli opendata comunali">
 
     <!-- Favicons -->
     <link rel="shortcut icon" href="https://www.comune.montelupo-fiorentino.fi.it<?= BASE_URL ?>assets/dist/favicon/favicon.ico">
