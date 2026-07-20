390ab1a04e238d0fb4453fc7888a07b599bd3d69
ComuneMontelupoFiorentino <e.gulli@comune.montelupo-fiorentino.fi.it>
2026-07-20 19:20:31 +0200

    Update notice_dataset.php

--- views/partials/notice_dataset.php
+++ views/partials/notice_dataset.php
@@ -1,4 +1,4 @@
-
+<!-- SECTION HERO-SEARCH (Note sui dati) -->
 <section class="hero-search py-4">
     <div class="container">
         <div class="row justify-content-center">
@@ -7,16 +7,15 @@
                     <div class="callout-inner">
                         <div class="callout-title">
                             <svg class="icon"><use href="assets/dist/svg/sprites.svg#it-info-circle"></use></svg>
-                            <span class="visually-hidden">Note</span> 
+                            <span class="visually-hidden">Note</span>
                             <span class="text">Note sui dati</span>
                         </div>
                         <span>Tutti i dataset usati in questo servizio sono consultabili e scaricabili in formato aperto sul 
-                            <a href="https://dati.toscana.it/organization/comune-di-montelupo-fiorentino" target="_blank">PORTALE OPENDATA<svg class="icon icon-primary icon-sm"><use href="assets/dist/svg/sprites.svg#it-external-link"></use></svg>
-                            </a>
+                            <a href="https://dati.toscana.it/organization/comune-di-montelupo-fiorentino" target="_blank" title="Vai a portale opendata">PORTALE OPENDATA<svg class="icon icon-primary icon-sm"><use href="assets/dist/svg/sprites.svg#it-external-link"></use></svg></a>
                         </span><br><br><br>
-                        <ul class="">
+                        <ul>
                             <li>
-                                <a class="list-item read-more" href="#" role="button" data-bs-toggle="modal" data-bs-target="#modalDataset">
+                                <a class="list-item read-more" href="#" role="button" data-bs-toggle="modal" data-bs-target="#modalDataset" title="Elenco dataset usati">
                                     <span class="span-custom read-more">Elenco dei Dataset usati in questo servizio
                                         <svg class="icon icon-sm icon-primary ms-1 mb-1"><use href="assets/dist/svg/sprites.svg#it-chart-line"></use></svg>
                                     </span>
@@ -38,14 +37,11 @@
                 <svg class="icon" aria-labelledby="titolo-svg" role="img"><title id="titolo-svg">Ricorda di inserire l'ID nel titolo o nei dettagli della segnalazione</title><use href="assets/dist/svg/sprites.svg#it-info-circle"></use></svg>
                 Dataset usati
                 <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Chiudi finestra modale">
-                    <svg class="icon"><use href="assets/dist/svg/sprites.svg#it-close"></use></svg>
-                </button>
+                    <svg class="icon"><use href="assets/dist/svg/sprites.svg#it-close"></use></svg>                </button>
             </div>
             <div class="modal-body">
                 <div class="link-list-wrapper">
-                    <ul class="link-list" id="list-dataset-used">
-                        
-                    </ul>
+                    <ul class="link-list" id="list-dataset-used"></ul>
                 </div>
             </div>
             <div class="modal-footer modal-footer-shadow">
